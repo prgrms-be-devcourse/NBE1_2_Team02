8245a53314de2ee850d2e68626e0e6339c89b04c
Taaate <an981022@naver.com>
2024-09-23 19:30:21 +0900

    Initial Commit

---
+++ src/main/java/com/example/book_your_seat/BookYourSeatApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_your_seat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourSeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourSeatApplication.class, args);
+	}
+
+}
