cd8c205cbe2e0bf6cc8b39400576df0113d26620
Taaate <an981022@naver.com>
2024-09-23 20:00:12 +0900

    feat: Address 도메인 코드 작성

--- src/main/java/com/example/book_your_seat/user/domain/User.java
+++ src/main/java/com/example/book_your_seat/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.example.book_your_seat.user.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,6 +8,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,11 +32,18 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private final List<Address> addressList = new ArrayList<>();
+
     public User(String nickname, String username, String email, String password) {
         this.nickname = nickname;
         this.username = username;
         this.email = email;
         this.password = password;
         this.userRole = UserRole.USER;
+    }
+
+    public void setAddress(Address address) {
+        this.addressList.add(address);
     }
 }
