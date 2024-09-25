e1ca4da5af937fe1f8e4c3c9e3fbf5a66a65e50e
kcsc2217 <k12002@nate.com>
2024-09-25 16:19:38 +0900

    feat : 해당 멤버가 쿠폰 받았는지 boolean

--- src/main/java/com/example/book_your_seat/coupon/repository/CouponRepository.java
+++ src/main/java/com/example/book_your_seat/coupon/repository/CouponRepository.java
@@ -4,4 +4,5 @@ import com.example.book_your_seat.coupon.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
 }
