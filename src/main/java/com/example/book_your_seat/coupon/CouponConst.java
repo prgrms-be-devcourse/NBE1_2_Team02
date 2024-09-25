0a0a0fd05f488069a2f43d1dfee4fe5610df799b
kcsc2217 <k12002@nate.com>
2024-09-25 16:17:40 +0900

    feat : 쿠폰 전역 상수 추가

--- src/main/java/com/example/book_your_seat/coupon/CouponConst.java
+++ src/main/java/com/example/book_your_seat/coupon/CouponConst.java
@@ -4,5 +4,7 @@ public final class CouponConst {
 
     public static final String STOCK_ZERO = "수량이 부족합니다";
 
+    public static final String NOTFOUND_COUPON = "쿠폰을 찾을수 없습니다";
+
     private CouponConst() {}
 }
