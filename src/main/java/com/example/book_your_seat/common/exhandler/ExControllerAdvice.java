2c2d9f8b0c184ad5b8ee1b9dde6759f2df1e48ca
kcsc2217 <k12002@nate.com>
2024-10-07 17:02:34 +0900

    feat: Slack 에러 메시지

--- src/main/java/com/example/book_your_seat/common/exhandler/ExControllerAdvice.java
+++ src/main/java/com/example/book_your_seat/common/exhandler/ExControllerAdvice.java
@@ -1,21 +1,40 @@
 package com.example.book_your_seat.common.exhandler;
 
 import com.example.book_your_seat.common.entity.ErrorResult;
+import com.example.book_your_seat.common.service.SlackService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
+
 public class ExControllerAdvice {
+
+    private final SlackService slackService;
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResult> catchError(IllegalArgumentException e) {
         log.error("[exceptionHandle] ex", e);
 
         ErrorResult errorResult = new ErrorResult(HttpStatus.BAD_REQUEST.name(), e.getMessage());
+        sendSlackErrorMessage(errorResult);
         return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    private void sendSlackErrorMessage(ErrorResult errorResult)
+     {
+        HashMap<String, String> dataMap = new HashMap<>();
+
+        dataMap.put("에러 로그", errorResult.message());
+
+        slackService.setErrorMessage(errorResult.code(), dataMap);
+
     }
 }
---
+++ src/main/java/com/example/book_your_seat/common/service/SlackService.java
@@ -0,0 +1,9 @@
+package com.example.book_your_seat.common.service;
+
+import java.util.HashMap;
+
+public interface SlackService {
+
+    public void setErrorMessage(String title, HashMap<String, String> data);
+
+}
