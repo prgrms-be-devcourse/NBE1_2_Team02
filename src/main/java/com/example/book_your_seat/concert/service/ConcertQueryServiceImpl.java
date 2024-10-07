85b21b57b168c165a6f7ef6c3d91777773fe410d
kcsc2217 <k12002@nate.com>
2024-10-07 12:47:28 +0900

    feat: 기능 옮기기3

--- src/main/java/com/example/book_your_seat/concert/service/ConcertQueryServiceImpl.java
+++ src/main/java/com/example/book_your_seat/concert/service/ConcertQueryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.book_your_seat.concert.service;
 
+import com.example.book_your_seat.concert.controller.dto.ConcertListResponse;
 import com.example.book_your_seat.concert.controller.dto.ConcertResponse;
+import com.example.book_your_seat.concert.controller.dto.ResultRedisConcert;
 import com.example.book_your_seat.concert.repository.ConcertRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,5 +32,23 @@ public class ConcertQueryServiceImpl implements ConcertQueryService {
         return concertRepository.findById(id)
                 .map(ConcertResponse::from)
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_CONCERT_ID + id));
+    }
+
+    @Override
+    public List<ConcertListResponse> findAllConcertList() {
+        return concertRepository.findAll()
+                .stream().map(ConcertListResponse::new)
+                .toList();
+    }
+
+
+    @Override
+    @Cacheable(cacheNames = "concerts", key = "'allConcerts'")
+    public ResultRedisConcert findUsedRedisAllConcertList() {
+        List<ConcertListResponse> list = concertRepository.findAll()
+                .stream().map(ConcertListResponse::new)
+                .toList();
+
+        return new ResultRedisConcert(list);
     }
 }
