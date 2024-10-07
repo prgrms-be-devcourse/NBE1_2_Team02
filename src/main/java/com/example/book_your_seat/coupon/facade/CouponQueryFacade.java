43e69afd95636ac17f064f891c4056c22181a816
김관현 <khan@gimgwanhyeon-ui-Macmini.local>
2024-10-07 17:45:27 +0900

    feat: 도우님 getPrice 기능 추가

--- src/main/java/com/example/book_your_seat/coupon/facade/CouponQueryFacade.java
+++ src/main/java/com/example/book_your_seat/coupon/facade/CouponQueryFacade.java
@@ -2,6 +2,7 @@ package com.example.book_your_seat.coupon.facade;
 
 import com.example.book_your_seat.coupon.controller.dto.CouponDetailResponse;
 import com.example.book_your_seat.coupon.domain.Coupon;
+import com.example.book_your_seat.coupon.domain.DiscountRate;
 import com.example.book_your_seat.coupon.domain.UserCoupon;
 import com.example.book_your_seat.coupon.manager.UserCouponManager;
 import com.example.book_your_seat.user.domain.User;
@@ -34,5 +35,12 @@ public class CouponQueryFacade implements CouponQueryService {
         UserCoupon userCoupon = userCouponManager.findValidUserCoupon(userCouponId);
         Coupon coupon = userCoupon.getCoupon();
         return CouponDetailResponse.fromCoupon(coupon);
+    }
+
+    @Override
+    public DiscountRate getDiscountRate(Long userCouponId) {
+        return userCouponManager.findValidUserCoupon(userCouponId)
+                .getCoupon()
+                .getDiscountRate();
     }
 }
--- src/test/java/com/example/book_your_seat/service/payment/PaymentServiceTest.java
+++ src/test/java/com/example/book_your_seat/service/payment/PaymentServiceTest.java
@@ -4,17 +4,22 @@ import com.example.DbCleaner;
 import com.example.book_your_seat.IntegralTestSupport;
 import com.example.book_your_seat.concert.domain.Concert;
 import com.example.book_your_seat.concert.repository.ConcertRepository;
+import com.example.book_your_seat.concert.service.ConcertCommandService;
 import com.example.book_your_seat.coupon.domain.Coupon;
 import com.example.book_your_seat.coupon.domain.DiscountRate;
 import com.example.book_your_seat.coupon.domain.UserCoupon;
 import com.example.book_your_seat.coupon.repository.CouponRepository;
 import com.example.book_your_seat.coupon.repository.UserCouponRepository;
+import com.example.book_your_seat.payment.controller.dto.request.FinalPriceRequest;
 import com.example.book_your_seat.payment.controller.dto.response.ConfirmResponse;
+import com.example.book_your_seat.payment.controller.dto.response.FinalPriceResponse;
 import com.example.book_your_seat.payment.controller.dto.response.TossConfirmResponse;
 import com.example.book_your_seat.payment.service.dto.PaymentCommand;
 import com.example.book_your_seat.payment.service.facade.PaymentFacade;
 import com.example.book_your_seat.reservation.contorller.dto.PaymentRequest;
 import com.example.book_your_seat.reservation.domain.ReservationStatus;
+import com.example.book_your_seat.seat.domain.Seat;
+import com.example.book_your_seat.seat.repository.SeatRepository;
 import com.example.book_your_seat.user.domain.Address;
 import com.example.book_your_seat.user.domain.User;
 import com.example.book_your_seat.user.repository.AddressRepository;
@@ -25,9 +30,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 class PaymentServiceTest extends IntegralTestSupport {
@@ -45,11 +55,39 @@ class PaymentServiceTest extends IntegralTestSupport {
     @Autowired
     private AddressRepository addressRepository;
     @Autowired
+    private SeatRepository seatRepository;
+
+    @Autowired
     private PaymentFacade paymentFacade;
+    @Autowired
+    private ConcertCommandService concertCommandService;
 
     @AfterEach
     void tearDown() {
         dbCleaner.cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("3333원 짜리 공연을 10%할인 쿠폰을 적용하여 최종금액 계산시 2999원")
+    void getFinalPriceTest() {
+        // given
+        User user = userRepository.save(new User("1234", "!234", "1234", "1234"));
+
+        Coupon coupon = couponRepository.save(new Coupon(1, DiscountRate.TEN, LocalDate.now().plusDays(30)));
+
+        UserCoupon userCoupon = userCouponRepository.save(new UserCoupon(user, coupon));
+
+        Concert concert = concertRepository.save(new Concert("1234", LocalDate.now(), LocalDate.now(), 3333, 1));
+
+        Seat seat = seatRepository.save(new Seat(concert));
+
+        FinalPriceRequest request = new FinalPriceRequest(Collections.singletonList(seat.getId()), userCoupon.getId());
+
+        // when
+        FinalPriceResponse response = paymentFacade.getFinalPrice(request);
+
+        // then
+        Assertions.assertThat(response.finalPrice()).isEqualTo(BigDecimal.valueOf(2999));
     }
 
     @DisplayName("Toss 에서 결제가 성공하면 결제 기록을 저장하고 결제된 정보를 사용자에게 전달한다")
