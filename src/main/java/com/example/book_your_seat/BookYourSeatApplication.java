922635a354b83442da5dd840629a525fe6c19a07
kcsc2217 <k12002@nate.com>
2024-10-07 09:38:32 +0900

    refactor: 어노테이션 위치조정

--- src/main/java/com/example/book_your_seat/BookYourSeatApplication.java
+++ src/main/java/com/example/book_your_seat/BookYourSeatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@EnableCaching
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableCaching
+
 public class BookYourSeatApplication {
 
 	public static void main(String[] args) {
