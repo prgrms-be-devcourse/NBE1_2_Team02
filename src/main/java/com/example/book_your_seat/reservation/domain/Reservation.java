7eb156a3425bffc5392a1d9b802ea25403e6cc97
An Tae ho <an981022@naver.com>
2024-11-01 11:18:37 +0900

    [FEAT] 럭키 드로우 구현 (#93)
    * feat: 럭키 드로우 구현
    
    * fix: 추출 방식 변경
    
    * fix: 추출 방식 변경
    
    * fix: 사소한 수정

--- src/main/java/com/example/book_your_seat/reservation/domain/Reservation.java
+++ src/main/java/com/example/book_your_seat/reservation/domain/Reservation.java
@@ -1,5 +1,6 @@
 package com.example.book_your_seat.reservation.domain;
 
+import com.example.book_your_seat.common.entity.BaseEntity;
 import com.example.book_your_seat.payment.domain.Payment;
 import com.example.book_your_seat.seat.domain.Seat;
 import com.example.book_your_seat.user.domain.Address;
@@ -16,7 +17,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Reservation {
+public class Reservation extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reservation_id")
@@ -39,7 +40,6 @@ public class Reservation {
 
     @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL)
     private final List<Seat> seats = new ArrayList<>();
-
 
     @Builder
     public Reservation(ReservationStatus status, User user, Address address,Payment payment) {
--- src/main/java/com/example/book_your_seat/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/book_your_seat/reservation/repository/ReservationRepository.java
@@ -1,7 +1,15 @@
 package com.example.book_your_seat.reservation.repository;
 
 import com.example.book_your_seat.reservation.domain.Reservation;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query(value = "SELECT r.user_id FROM Reservation r WHERE r.status = 'SHIPPED' AND r.last_modified_at BETWEEN :startDate AND :endDate ORDER BY RAND() LIMIT 50", nativeQuery = true)
+    List<Long> findShippedReservationsLastMonth(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 }
