67391b70d8aff317e888232b16d8c6522c59e3ef
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-11-04 09:50:45 +0900

    refactor: 대기열 리팩토링 (#98)
    * refactor: 대기열 리팩토링
    - userId 중복 검사 삭제
    - 콘서트 별 대기열 생성
    
    * fix: 스케줄러 업데이트 시간 3초로 변경
    
    * chore: import 정리
    
    * test: test code 수정

--- src/main/java/com/example/book_your_seat/seat/controller/dto/SelectSeatRequest.java
+++ src/main/java/com/example/book_your_seat/seat/controller/dto/SelectSeatRequest.java
@@ -4,10 +4,13 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-import static com.example.book_your_seat.seat.SeatConst.ENTER_SEATS;
+import static com.example.book_your_seat.seat.SeatConst.*;
 
 public record SelectSeatRequest(
-
+        @NotNull(message = ENTER_QUEUE_TOKEN )
+        String queueToken,
+        @NotNull(message = ENTER_CONCERT_ID )
+        Long concertId,
         @NotNull(message = ENTER_SEATS)
         List<Long> seatIds
 ) {
