db28a468d1ff83df039f82e3fb203ff3668b10e6
kwanse <khan67781397@gmail.com>
2024-11-05 14:34:37 +0900

    fix: seat bulk insert 수정

--- src/main/java/com/example/book_your_seat/seat/repository/SeatJdbcRepositoryImpl.java
+++ src/main/java/com/example/book_your_seat/seat/repository/SeatJdbcRepositoryImpl.java
@@ -1,23 +1,34 @@
 package com.example.book_your_seat.seat.repository;
 
+import com.example.book_your_seat.concert.domain.Concert;
 import com.example.book_your_seat.seat.domain.Seat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class SeatJdbcRepositoryImpl implements SeatJdbcRepository {
 
-    private final JdbcClient jdbcClient;
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public void insertBulkSeats(List<Seat> seats) {
-        String sql = "INSERT INTO Seat (concertId, seatNumber, zone, isSold) " +
-                "VALUES (:concertId, :seatNumber, :zone, :isSold)";
+        String sql = "INSERT INTO seat (concert_id, seat_number, zone, is_sold) " +
+                "VALUES (?, ?, ?, ?)";
 
-        jdbcClient.sql(sql).params(seats).update();
+        jdbcTemplate.batchUpdate(sql,
+                seats,
+                seats.size(),
+                (PreparedStatement ps, Seat seat) -> {
+                    ps.setLong(1, seat.getId().getConcertId());
+                    ps.setLong(2, seat.getId().getSeatNumber());
+                    ps.setString(3, seat.getZone().toString());
+                    ps.setBoolean(4, false);
+                });
     }
 }
